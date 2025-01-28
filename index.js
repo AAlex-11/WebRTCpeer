a75e9793f803a8d54fe0a9c424563eadadffdddf
Razen <Razen13371@outlook.com>
2025-01-28 13:18:24 +0200

    empty express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+
+// WebRTC-specific logic 
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
